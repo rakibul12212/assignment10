723a97a38fb62c9be2045709be0fa162d1331cd1
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-05-02 16:16:24 +0600

    react-router-setup

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes';
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+   <RouterProvider router={router} ><App></App></RouterProvider>
+  </React.StrictMode>,
+)
